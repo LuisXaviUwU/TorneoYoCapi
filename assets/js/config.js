a37051ed59f0d5c0ba23ef5d6dd1dd283230629a
Luis_Xavi_Uwu <42522375+LuisXaviUwU@users.noreply.github.com>
2026-07-22 21:27:03 -0600

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@ window.SUPABASE_URL  = 'https://jwkfihuidydpsaqkpucp.supabase.co';
 window.SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3a2ZpaHVpZHlkcHNhcWtwdWNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1Nzg3NTksImV4cCI6MjEwMDE1NDc1OX0.RKMDbvRnMzSstqfIu7vNc86zRKAWpAzgYW4NrU0HlAU';
 
 // Backend Python (local o Railway/Render)
-window.TOURNAMENT_API_BASE = window.TOURNAMENT_API_BASE
+window.TOURNAMENT_API_BASE = 'https://torneoyocapi.onrender.com/api';
   || (location.hostname.endsWith('github.io')
     ? ''   // En GitHub Pages solo usamos Supabase directo
     : `${location.origin}/api`);
